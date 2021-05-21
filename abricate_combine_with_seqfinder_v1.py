1e2fc5c3824a5af720b61df192366fefc5ba471e
Javi <javier.nunezgarcia@apha.gov.uk>
2021-05-21 17:41:45 +0100

    21052021_new combination filtering

--- abricate_combine_with_seqfinder_v1.py
+++ abricate_combine_with_seqfinder_v1.py
@@ -10,6 +10,7 @@ import sys
 import pandas as pd
 import warnings
 warnings.simplefilter(action='ignore', category=UserWarning)
+
 #Display entire dataframe on console
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.width', 250)
@@ -18,16 +19,25 @@ pd.set_option('display.width', 250)
 #print (df_abr)
 #df_sf = pd.read_csv('/home/p312523/mnt/RDVM0529/VM0529/2017/caesbl/amr/original_output_file/150_good_snps.csv')
 #df_sf = pd.read_csv('/home/p312523/mnt/AMRCoreEvid_VM0533/Raw_Seq_Reads_DO.NOT.EDIT/1.Mixed_August_2019/fastqs/fastqs/Seqfinder_output/fastqs/AMRDatabase_20190702_and_EnteroPLasmids_20190514/1.seqfinder_output/BL301_good_snps.csv')
+
+#df_abr = pd.read_csv("/home/javi/WGS_Results/Project_1/Solveig_Mo_98-2012-01-1657_S26/Solveig_Mo_98-2012-01-1657_S26.abricate", sep='\t')
+#df_sf = pd.read_csv("/home/javi/WGS_Results/Project_1/Solveig_Mo_98-2012-01-1657_S26/Solveig_Mo_98-2012-01-1657_S26_CompareTo_AMRDatabase_20200729_and_EnteroPLasmids_20190514_short-tetA6_good_snps.csv")
+#df_abr.drop(["ACCESSION","COVERAGE","#FILE","DATABASE","COVERAGE_MAP"],axis=1,inplace=True)
 df_abr = pd.read_csv(sys.argv[1], sep='\t')
 df_sf = pd.read_csv(sys.argv[2])
+
+###grouping plasmids by contig and choosing a representative 
 #Make list of genes from the SeqFinder output
 df_abr["LOCATION"]=df_abr['START'].astype(str)+".."+df_abr['END'].astype(str)
+
 plasmids_only=df_abr[(df_abr.GENE.str.contains("PlasR"))]
 grouped_plasmids = plasmids_only.loc[plasmids_only.groupby("SEQUENCE")["%COVERAGE"].idxmax()]
+#### removed indexes with plasmids and merging the rest with the representatives
 drop_plasmids=df_abr[(~df_abr.GENE.str.contains("PlasR"))]
 merge_grouped_plasmids=drop_plasmids,grouped_plasmids
 df_abr=pd.concat(merge_grouped_plasmids)
-df_sf_genes= (df_sf['gene'])
+
+
 #Circumvents the filtering rule that would exclude ant3
 ant3_rule = df_abr[(df_abr.GENE.str.contains("ant3") & (df_abr['%COVERAGE'] >=80))]
 #Drop hits for chromosomal genes from the abricate output, ones less than 90% coverage and identity, and those that are plasmid genes
@@ -38,31 +48,42 @@ merge_ant3 = filtering_rule,ant3_rule
 filtering_rule = pd.concat(merge_ant3)
 #Use same rule as SeqFinder to make a 4 character gene code from the abricate hits and place into a new column called GENE_id
 df_abr_GENE = filtering_rule['GENE']
+
 gene_prefixes=list()
 for i in df_abr_GENE:
     s = i.find('_')
     gene_prefixes.append(i[s+1:s+5])
 filtering_rule['GENE_id']=gene_prefixes
+
+
 #Filter the GENE_id list with the ones found in the SeqFinder dataframe
+df_sf_genes= (df_sf['gene'])
 filter_for_sf_genes = filtering_rule.loc[filtering_rule['GENE_id'].isin(df_sf_genes)]
 GENE_id = (filter_for_sf_genes['GENE_id'])
+
 #Create a dictionary of filtered GENE_id hits and the GENE hit
 grouped_df_gene= filter_for_sf_genes.groupby('GENE_id')['GENE'].apply(','.join).reset_index()
 abricate_dictionary_gene = grouped_df_gene.set_index('GENE_id').T.to_dict('list')
+
 abricate_dictionary_sequence = filter_for_sf_genes[['GENE','SEQUENCE']]
-abricate_dictionary_coverage = filter_for_sf_genes[['GENE','%COVERAGE']]
-abricate_dictionary_identity = filter_for_sf_genes[['GENE','%IDENTITY']]
-abricate_dictionary_location = filter_for_sf_genes[['GENE','LOCATION']]
 abricate_dictionary_sequence = abricate_dictionary_sequence.set_index('GENE').T.to_dict('list')
+
+abricate_dictionary_coverage = filter_for_sf_genes[['GENE','%COVERAGE']]
 abricate_dictionary_coverage = abricate_dictionary_coverage.set_index('GENE').T.to_dict('list')
+
+abricate_dictionary_identity = filter_for_sf_genes[['GENE','%IDENTITY']]
 abricate_dictionary_identity = abricate_dictionary_identity.set_index('GENE').T.to_dict('list')
+
+abricate_dictionary_location = filter_for_sf_genes[['GENE','LOCATION']]
 abricate_dictionary_location = abricate_dictionary_location.set_index('GENE').T.to_dict('list')
+
 #Map the dictionarys onto the SeqFinder dataframe into columns listed below
 df_sf['result_abr']= df_sf['gene'].map(abricate_dictionary_gene)
 df_sf['contig_abr']= df_sf['id'].map(abricate_dictionary_sequence)
 df_sf['coverage_abr']= df_sf['id'].map(abricate_dictionary_coverage)
 df_sf['identity_abr']= df_sf['id'].map(abricate_dictionary_identity)
 df_sf['location_abr']= df_sf['id'].map(abricate_dictionary_location)
+
 #Clean up the abricate columns to only include the text and no brackets
 df_sf['result_abr'] = df_sf['result_abr'].map(lambda x: str(x)[2:-2])
 df_sf['contig_abr'] = df_sf['contig_abr'].map(lambda x: str(x)[2:-2])
@@ -71,6 +92,7 @@ df_sf['identity_abr'] = df_sf['identity_abr'].map(lambda x: str(x)[1:-1])
 df_sf['location_abr'] = df_sf['location_abr'].map(lambda x: str(x)[2:-2])
 #Replace 'a' (a carry over from NaN) with blanks
 df_sf=df_sf.replace({'a':''})
+
 #Pick the plasmid rep gene with the highest coverage as the one to report
 list_of_contigs = df_sf['contig_abr']
 filter_for_abr_contigs = df_abr.loc[df_abr['SEQUENCE'].isin(list_of_contigs)]
@@ -78,16 +100,17 @@ filter_for_abr_contigs = filter_for_abr_contigs[(df_abr.GENE.str.contains("PlasR
 grouped_plasmids = df_abr.groupby(by="SEQUENCE")['%COVERAGE'].max()
 abricate_dictionary_plasmid = filter_for_abr_contigs[['SEQUENCE','GENE']]
 abricate_dictionary_plasmid = abricate_dictionary_plasmid.set_index('SEQUENCE').T.to_dict('list')
+
+
 df_sf['plasmid_abr']= df_sf['contig_abr'].map(abricate_dictionary_plasmid)
 df_sf['plasmid_abr'] = df_sf['plasmid_abr'].map(lambda x: str(x)[2:-2])
+
 #Reformat the output columns in the order we desire
 abricate_columns=(df_sf.loc[:,['plasmid_abr','coverage_abr','identity_abr','location_abr']])
 df_sf=df_sf.iloc[:,:-4]
 merge=df_sf,abricate_columns
 df_sf=pd.concat([df_sf, abricate_columns], axis=1, sort=False)
-#print(df_sf)
-#Determine the output filename
-output_filename = sys.argv[2].split('.')[0] + '_abricate_seqfinder.csv'
+output_filename = sys.argv[2].split('.')[0] + '_abricate_seqfinder_current_version.csv'
 df_sf.to_csv(output_filename,index=False)
 print('Done! Saved output as {}.'.format(output_filename))
 ###To automatically solve the CTX, TEM and CMY multiple occurance issue this can be turned on
@@ -96,3 +119,31 @@ print('Done! Saved output as {}.'.format(output_filename))
 #print(filter_for_TEM)
 #grouped_df = filter_for_TEM.groupby(by="gene")['snps'].min()
 #print(grouped_df)
+
+
+
+###javi filtering #########################
+combi=df_sf.values.tolist()
+cols=df_sf.columns.tolist()
+contig_abr_p=cols.index('contig_abr')
+gene_abr_p=cols.index('gene')
+result_abr_p=cols.index('result_abr')
+
+abr_contigs_plasmids=[x[result_abr_p].split("_")[1][:4] for x in combi if x[contig_abr_p]!=""]
+
+new_sf=[]
+for row in combi:
+    if row[contig_abr_p]!="":
+        new_sf.append(row)
+        continue
+    if row[gene_abr_p] not in abr_contigs_plasmids:
+        new_sf.append(row)
+df_new_sf = pd.DataFrame.from_records(new_sf)
+df_new_sf.columns=cols
+###########################################
+#Determine the output filename
+output_filename = sys.argv[2].split('.')[0] + '_abricate_seqfinder.csv'
+df_new_sf.to_csv(output_filename,index=False)
+
+
+
